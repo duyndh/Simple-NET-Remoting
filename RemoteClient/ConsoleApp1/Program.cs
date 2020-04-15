ccab11882beef6f6c95db06881d569f7fc1547df
duyndh <hoangduypok98@gmail.com>
2020-04-15 22:44:49 +0700

    Demo

---
+++ RemoteClient/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TcpChannel ch = new TcpChannel();
+            while (true)
+            {
+                RemoteClass.RemoteClass obj = new RemoteClass.RemoteClass();
+                obj = (RemoteClass.RemoteClass)Activator.GetObject(typeof(RemoteClass.RemoteClass), "tcp://localhost:8085/test");
+
+                var inputStr = Console.ReadLine();
+
+                Console.Write("x = ");
+                int x = Int32.Parse(Console.ReadLine());
+                Console.Write("y = ");
+                int y = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("x + y = " + obj.Sum(x, y).ToString());
+            }
+        }
+    }
+}
---
+++ RemoteServer/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel ch = new TcpChannel(8085);
+
+            ChannelServices.RegisterChannel(ch);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(RemoteClass.RemoteClass), "test", WellKnownObjectMode.Singleton);
+
+            Console.Write("Sever is ready ...");
+
+            Console.Read();
+        }
+    }
+}
