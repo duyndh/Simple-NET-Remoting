7e56f6b3205481bfdc407b0267294c9dbe5e94c1
duyndh <hoangduypok98@gmail.com>
2020-04-29 04:59:16 +0700

    Update new demo location

---
+++ Demo1/RemoteClient/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿// SingleCall / Singleton / ClientAO 
+#define Singleton
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+
+
+namespace RemoteClient
+{
+    class Program
+    {
+        private const string serverIp = "192.168.11.102";
+        private const int PORT = 8089;
+        private const string APP_NAME = "test";
+
+
+        static void Main(string[] args)
+        {
+#if !(ClientAO)
+
+            // Server Activated Objects
+            var remoteObject = (RemoteClass.RemoteClass)Activator.GetObject(
+                typeof(RemoteClass.RemoteClass),
+                string.Format("tcp://{0}:{1}/{2}", serverIp, PORT.ToString(), APP_NAME));
+
+#else
+            // Client Activated Objects
+            RemotingConfiguration.RegisterActivatedClientType(
+                typeof(RemoteClass.RemoteClass),
+                string.Format("tcp://{0}:{1}/{2}", serverIp, PORT.ToString(), APP_NAME));
+            var remoteObject = new RemoteClass.RemoteClass();
+#endif
+
+            while (true)
+            {                
+                Console.WriteLine(remoteObject.Ping());
+                Console.WriteLine("n call: " + remoteObject.GetCount().ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
